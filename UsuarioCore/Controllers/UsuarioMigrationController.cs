1e2a6cc607b5cfd9426d36715fe0fc911b298d8a
Francisco Batalla <falfonsob01@outlook.com>
2025-07-09 10:16:44 -0600

    GetAll y vista funcionando

--- UsuarioCore/Controllers/UsuarioMigrationController.cs
+++ UsuarioCore/Controllers/UsuarioMigrationController.cs
@@ -18,30 +18,21 @@ namespace PL.Controllers
 
         public IActionResult TablaGetAll()
         {
-            ML.Usuario usuario = new ML.Usuario();
-            usuario.Rol = new ML.Rol();
+            ML.Usuario user = new ML.Usuario();
+            user.Rol = new ML.Rol();
+            user.Direccion ??= new ML.Direccion();
+            user.Direccion.Colonia ??= new ML.Colonia();
+            user.Direccion.Colonia.Municipio ??= new ML.Municipio();
+            user.Direccion.Colonia.Municipio.Estado ??= new ML.Estado();
 
             ML.Result resultRol = _context.GetAllRol();
             if (resultRol.Correct)
             {
-                usuario.Rol.Roles = resultRol.Objects;
+                user.Rol.Roles = resultRol.Objects;
             }
 
 
-            ML.Result resultGetAll = _context.GetAll(usuario);
-
-
-            if (resultGetAll.Objects != null)
-            {
-                foreach (ML.Usuario user in resultGetAll.Objects)
-                {
-                    user.Rol ??= new ML.Rol();
-                    user.Direccion ??= new ML.Direccion();
-                    user.Direccion.Colonia ??= new ML.Colonia();
-                    user.Direccion.Colonia.Municipio ??= new ML.Municipio();
-                    user.Direccion.Colonia.Municipio.Estado ??= new ML.Estado();
-                }
-            }
+            ML.Result resultGetAll = _context.GetAll(user);
 
             return View(resultGetAll);
         }
